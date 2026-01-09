397fc4381cc24a863bd266bdf9b29757d6b00a80
POTHANA PARDHU <pardhupothana2004@gmail.com>
2026-01-09 19:22:01 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,17 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://lms-portal-z271.onrender.com/api' 
+    baseURL: 'https://lms-portal-z271.onrender.com/api'
+});
+
+// Add this interceptor to attach the token to every request
+API.interceptors.request.use((req) => {
+    const profile = localStorage.getItem('profile'); // Or whatever key you use to store user data
+    if (profile) {
+        const token = JSON.parse(profile).token;
+        req.headers.Authorization = `Bearer ${token}`;
+    }
+    return req;
 });
 
 export default API;
